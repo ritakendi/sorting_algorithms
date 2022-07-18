d86c37dbf50dc96ea0471f5ab03e6c76e80f3d4b
ritakendi <ritabetty77@gmail.com>
2022-07-18 21:19:32 +0300

    trial'

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * bubble_sort - sorts an array of integers in ascending order using
- * the Bubble sort algorithm
+ * the Bubble sort lgorithm
  * @array: pointer to array
  * @size: size of the array
  **/
