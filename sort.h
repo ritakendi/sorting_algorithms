abbb66ec482f4b21fffea415bb27e6c04860a0e9
ritakendi <ritabetty77@gmail.com>
2022-07-18 21:22:38 +0300

    updated sort.h

--- sort.h
+++ sort.h
@@ -19,5 +19,13 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void print_list_rev(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 #endif
